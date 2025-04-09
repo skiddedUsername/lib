883bd488e637a8e76556d00fa3f91b06479fede1
skiddedUsername <grizzleot@icloud.com>
2025-04-08 22:05:00 -0400

    Update source.lua

--- source.lua
+++ source.lua
@@ -638,7 +638,7 @@ repeat
 		warned = true
 	end
 
-	toDestroy, Rayfield = Rayfield, useStudio and script.Parent:FindFirstChild('Rayfield') or game:GetObjects("rbxassetid://84701656902538")[1]
+	toDestroy, Rayfield = Rayfield, useStudio and script.Parent:FindFirstChild('Rayfield') or game:GetObjects("rbxassetid://10804731440")[1]
 	if toDestroy and not useStudio then toDestroy:Destroy() end
 
 	buildAttempts = buildAttempts + 1
